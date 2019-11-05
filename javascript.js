f1f69c6ab965ec7dd66379bc90bebfa9426703aa
akseonov <konstantinaxeonov@gmail.com>
2019-11-05 13:54:06 +0500

    - сделан вывод корзины на странице корзины (shopping_cart.html), пока коряво

--- javascript.js
+++ javascript.js
@@ -23,12 +23,9 @@ class ItemsList {
         });
     }
 
-    render() {
-        if (this.loaded && this.filteredItems.length === 0) {
-            return `<div>Ничего не найдено</div>`;
-        }
-
-        return this.filteredItems.map((item) => new Item(item.id, item.title, item.price, item.url).render()).join('');
+    render(size) {
+        return this.filteredItems.slice(0,size).map((item) =>
+                new Item(item.id, item.title, item.price, item.url).render()).join('');
     }
 }
 
@@ -41,13 +38,13 @@ class Item {
     }
 
     render() {
-        return `<div class="products__fetured__item">
+        return `<article class="products__fetured__item">
             <a href="#">
                 <img src="${this.url}" alt="${this.url}">
             </a>
             <div class="futured__info">
                 <a class="futured__a" href="single_page.html">${this.title}</a>
-                <h4 class="fetured__h4">$ ${this.price}</h4>
+                <h4 class="fetured__h4">$ ${this.price}.00</h4>
             </div>
             <div class="fetured__add">
                 <button class="fetured__a"
@@ -64,109 +61,137 @@ class Item {
             <div class="fetured__add">
                 <a href="#" class="fetured__a_3"></a>
             </div>
-        </div>`
+        </article>`
     }
 }
 
-// class Cart {
-//     constructor() {
-//         this.items = [];
-//         this.element = null;
-//     }
-//
-//     fetchItems() {
-//         return fetch('/cart')
-//             .then(response => response.json())
-//             .then((items) => {
-//                 this.items = items;
-//             });
-//     }
-//
-//     add(item) {
-//         fetch('/cart', {
-//             method: 'POST',
-//             body: JSON.stringify({...item, qty: 1}),
-//             headers: {
-//                 'Content-type': 'application/json',
-//             },
-//         })
-//             .then((response) => response.json())
-//             .then((item) => {
-//                 this.element.insertAdjacentHTML('beforeend', this.renderItem(item));
-//             });
-//         this.items.push({...item, qty: 1});
-//     }
-//
-//     update(id, newQty) {
-//         if(newQty < 1) {
-//             if(confirm('Вы действительно хотите удалить товар из корзины?')) {
-//                 fetch(`/cart/${id}`, {
-//                     method: 'DELETE',
-//                 })
-//                     .then(response => response.json())
-//                     .then((item) => {
-//                         const $item = document.querySelector(`.cart li[data-id="${id}"]`);
-//                         if($item) {
-//                             $item.remove();
-//                         }
-//                     });
-//                 const idx = this.items.findIndex(entity => entity.id === id);
-//                 this.items.splice(idx, 1);
-//             } else {
-//                 return false;
-//             }
-//         } else {
-//             fetch(`/cart/${id}`, {
-//                 method: 'PATCH',
-//                 body: JSON.stringify({qty: newQty}),
-//                 headers: {
-//                     'Content-type': 'application/json',
-//                 },
-//             })
-//                 .then(response => response.json())
-//                 .then((item) => {
-//                     console.log('Обновление количества прошло успешно!');
-//                 });
-//
-//             const idx = this.items.findIndex(entity => entity.id === id);
-//             this.items[idx].qty = newQty;
-//         }
-//
-//         return true;
-//     }
-//
-//     renderItem(item) {
-//         return `<li data-id="${item.id}">
-//         <h3>${item.title}</h3>
-//         <input class="qty" type="number" value="${item.qty}" />
-//       </li>`
-//     }
-//
-//     render() {
-//         if(!this.element) {
-//             this.element = document.createElement('ul');
-//
-//             this.element.innerHTML = this.items.map(this.renderItem).join('');
-//         }
-//
-//         return this.element;
-//     }
-//
-//     total() {
-//         return this.items.reduce((acc, item) => acc + item.qty * item.price, 0);
-//     }
-// }
+class Cart {
+    constructor() {
+        this.items = [];
+        this.element = null;
+    }
+
+    fetchItems() {
+        return fetch('/cart')
+            .then(response => response.json())
+            .then((items) => {
+                this.items = items;
+            });
+    }
+
+    add(item) {
+        fetch('/cart', {
+            method: 'POST',
+            body: JSON.stringify({...item, qty: 1}),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((item) => {
+                this.element.insertAdjacentHTML('beforeend', this.renderItem(item));
+            });
+        this.items.push({...item, qty: 1});
+    }
+
+    update(id, newQty) {
+        if(newQty < 1) {
+            if(confirm('Вы действительно хотите удалить товар из корзины?')) {
+                fetch(`/cart/${id}`, {
+                    method: 'DELETE',
+                })
+                    .then(response => response.json())
+                    .then((item) => {
+                        const $item = document.querySelector(`.cart li[data-id="${id}"]`);
+                        if($item) {
+                            $item.remove();
+                        }
+                    });
+                const idx = this.items.findIndex(entity => entity.id === id);
+                this.items.splice(idx, 1);
+            } else {
+                return false;
+            }
+        } else {
+            fetch(`/cart/${id}`, {
+                method: 'PATCH',
+                body: JSON.stringify({qty: newQty}),
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            })
+                .then(response => response.json())
+                .then((item) => {
+                    console.log('Обновление количества прошло успешно!');
+                });
+
+            const idx = this.items.findIndex(entity => entity.id === id);
+            this.items[idx].qty = newQty;
+        }
+
+        return true;
+    }
+
+    renderItem(item) {
+        return `<div class="shopping_cart__item">
+            <div class="shopping_cart_box1">
+                <div class="shopping_cart_box1_img">
+                    <img class="shopping_cart_img" src="${item.url}" alt="${item.url}">
+                </div>
+                <div class="shopping_cart_box1_p_cont">
+                <p class="shopping_cart_box1_p">${item.title}</p>
+                <p class="shopping_cart_box1_p1"><span class="shopping_cart_box1_span">Color:</span>   Red</p>
+                <p class="shopping_cart_box1_p1"><span class="shopping_cart_box1_span">Size:</span>   Xll</p>
+                </div>
+            </div>
+            <div class="shopping_cart_box2 shopping_cart_center">
+                <p class="shopping_cart_box1_p2">$${item.price}</p>
+            </div>
+            <div class="shopping_cart_box3 shopping_cart_center">
+                <input class="shopping_cart_box1_input" type="text" placeholder="${item.qty}" pattern="\\S+[1-100]">
+            </div>
+            <div class="shopping_cart_box4 shopping_cart_center">
+                <p class="shopping_cart_box1_p3">FREE</p>
+            </div>
+            <div class="shopping_cart_box5 shopping_cart_center">
+                <p class="shopping_cart_box1_p2">$${item.price*item.qty}</p>
+            </div>
+            <div class="shopping_cart_box6 shopping_cart_center">
+                <div class="shopping_cart_box6_i"></div>
+            </div>
+        </div>`
+    }
+    //<li data-id="${item.id}">
+    //         <h3>${item.title}</h3>
+    //         <input class="qty" type="number" value="${item.qty}" />
+    //       </li>
+    render() {
+        this.element = document.querySelector('.shopping_cart');
+        this.element.innerHTML = this.items.map(this.renderItem).join('');
+
+        return this.element;
+    }
+
+    total() {
+        return this.items.reduce((acc, item) => acc + item.qty * item.price, 0);
+    }
+}
 
 const items = new ItemsList();
 items.fetchItems().then(() => {
-    document.querySelector('.feature__box').innerHTML = items.render();
+    document.querySelector('.feature__box').innerHTML = items.render(9);
 });
 
-// const cart = new Cart();
-// cart.fetchItems().then(() => {
-//     document.querySelector('.cart').appendChild(cart.render());
-//     document.querySelector('.total').innerHTML = cart.total();
-// });
+const items2 = new ItemsList();
+items2.fetchItems().then(() => {
+    document.querySelector('.fetured__content').innerHTML = items2.render(8);
+});
+
+const cart = new Cart();
+cart.fetchItems().then(() => {
+    document.querySelector('.shopping_cart').appendChild(cart.render());
+    // document.querySelector('.total').innerHTML = cart.total();
+});
 
 document.querySelector('.cart').addEventListener('change', (event) => {
     if (event.target.classList.contains('qty')) {
@@ -192,6 +217,8 @@ document.querySelector('.feature__box').addEventListener('click', (event) => {
         document.querySelector('.total').innerHTML = cart.total();
     }
 });
+
+
 
 document.querySelector('[name="query"]').addEventListener('input', (event) => {
     const query = event.target.value;
